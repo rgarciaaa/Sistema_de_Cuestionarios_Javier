722438e8f904a6e04c2a940d5f91354967711ef7
rgarciaa <renega30nov99@gmail.com>
2021-06-14 06:55:45 -0500

    Proyecto Final

--- server/server.js
+++ server/server.js
@@ -28,30 +28,31 @@ app.get('/', (req, res) => {
 
 app.post('/authUser', (req, res) => {
     console.log(chalk.bgBlue.white("Service required authUser "));
+    console.log(req.body);
 
     // Autentica al usuario
-    const User = req.body;
+    const { user, password } = req.body;
 
     connect.then(conn => {
-        conn.query(`SELECT * FROM usuario WHERE user_name = ?`, User.user)
+        conn.query(`SELECT * FROM usuario WHERE usuario.user_name = ${JSON.stringify(user)}`)
             .then(rows => {
-                if(rows.length > 0) {
+                if (rows.length > 0) {
                     // Si existe el usuario en la DB
                     // Comprobamos que la contraseña sea correcta
-                    if(rows[0].user_password === User.password) {
+                    if (rows[0].user_password === password) {
                         // Las credenciales son correctas
                         // Damos acceso al sistema
-                        res.json({ auth: true , ...rows[0] })
-                    }else {
+                        res.json({ auth: true, ...rows[0] })
+                    } else {
                         // Las credenciales no son correctas 
                         // Negamos el acceso al sistema
-                        res.json({ auth: false,error: "Contraseña incorrecta", ...rows[0] })
+                        res.json({ auth: false, error: "Contraseña incorrecta", ...rows[0] })
                     }
-                    
-                }else {
+
+                } else {
                     // No existe el usuario en la base de datos
                     // Negamos el acceso al sistema
-                    res.json({ auth: false,error: "Usuario y/o contraseña incorrectos", ...rows[0] })
+                    res.json({ auth: false, error: "Usuario y/o contraseña incorrectos", ...rows[0] })
                 }
             })
     })
@@ -83,25 +84,25 @@ app.get('/getCat', (req, res) => {
 
 app.post('/saveCuest', (req, res) => {
     // Separamos el JSON en cuestionario y preguntas
-    const {nombre, fecha, categoria}  = req.body.cuestionario;
+    const { nombre, fecha, categoria } = req.body.cuestionario;
     const preguntas = req.body.preguntas;
     // Lanzamos el query a la base de datos para guardar el cuestionario
     connect.then((conn) => {
         // Guardamos el cuestionario
-        conn.query(`INSERT INTO cuestionario VALUES (NULL,${JSON.stringify(nombre)},${JSON.stringify(fecha)},${JSON.stringify(categoria)});`) 
-        .then((rows) => {
-            // Recuperamos el id del cuestionario creado
-            const cuestionarioID = rows.insertId;
-            // Guardamos las preguntas relacionadas al cuestionario
-            preguntas.forEach(pregunta => {
-                conn.query(`INSERT INTO cuest_pregunta VALUES (NULL,${JSON.stringify(cuestionarioID)},${JSON.stringify(pregunta.id)});`);
-            });
-            // Respondemos que las preguntas se guardaron con exito
-            res.send({ succeed: true })
-        }).catch(error => {
-            // Respondemos que las preguntas no se pudieron guardar
-            res.send({ succeed: false })
-        })
+        conn.query(`INSERT INTO cuestionario VALUES (NULL,${JSON.stringify(nombre)},${JSON.stringify(fecha)},${JSON.stringify(categoria)});`)
+            .then((rows) => {
+                // Recuperamos el id del cuestionario creado
+                const cuestionarioID = rows.insertId;
+                // Guardamos las preguntas relacionadas al cuestionario
+                preguntas.forEach(pregunta => {
+                    conn.query(`INSERT INTO cuest_pregunta VALUES (NULL,${JSON.stringify(cuestionarioID)},${JSON.stringify(pregunta.id)});`);
+                });
+                // Respondemos que las preguntas se guardaron con exito
+                res.send({ succeed: true })
+            }).catch(error => {
+                // Respondemos que las preguntas no se pudieron guardar
+                res.send({ succeed: false })
+            })
     })
 })
 
@@ -127,14 +128,69 @@ app.post('/pullQuest', (req, res) => {
     })
 })
 
-app.get('/getCuest', (req,res) => {
+app.get('/estadisticas', (req, res) => {
+    // Recuperamos el numero de alumnos inscritos por cuestionario
+    connect.then(conn => {
+        // Cuestionario A
+        conn.query(`SELECT COUNT(*) totalA, SUM(cal_cuestionario) calificacionA FROM ins_per_cuest WHERE id_cuestionario = 58;`)
+            .then(rows => {
+                const { totalA, calificacionA } = rows[0];
+                conn.query('SELECT COUNT(*) aciertosA FROM resp_per_cuest NATURAL INNER JOIN cuest_pregunta WHERE valor = 1 AND id_cuestionario = 58;')
+                    .then(rows => {
+                        const aciertosA = rows[0].aciertosA
+                        // Cuestionario B
+                        conn.query(`SELECT COUNT(*) totalB, SUM(cal_cuestionario) calificacionB FROM ins_per_cuest WHERE id_cuestionario = 59;`)
+                            .then(rows => {
+                                const { totalB, calificacionB } = rows[0];
+                                conn.query('SELECT COUNT(*) aciertosB FROM resp_per_cuest NATURAL INNER JOIN cuest_pregunta WHERE valor = 1 AND id_cuestionario = 59;')
+                                    .then(rows => {
+                                        const aciertosB = rows[0].aciertosB
+                                        // Cuestionario C
+                                        conn.query(`SELECT COUNT(*) totalC, SUM(cal_cuestionario) calificacionC FROM ins_per_cuest WHERE id_cuestionario = 60;`)
+                                            .then(rows => {
+                                                const { totalC, calificacionC } = rows[0];
+                                                conn.query('SELECT COUNT(*) aciertosC FROM resp_per_cuest NATURAL INNER JOIN cuest_pregunta WHERE resp_per_cuest.valor = 1 AND id_cuestionario = 60;')
+                                                    .then(rows => {
+                                                        const aciertosC = rows[0].aciertosC
+                                                        // Empaquetamos y enviamos
+                                                        const json = {
+                                                            cuestA: {
+                                                                totalA,
+                                                                calificacionA,
+                                                                aciertosA
+                                                            },
+                                                            cuestB: {
+                                                                totalB,
+                                                                calificacionB,
+                                                                aciertosB
+                                                            },
+                                                            cuestC: {
+                                                                totalC,
+                                                                calificacionC,
+                                                                aciertosC
+                                                            }
+                                                        };
+                                                        res.send(json);
+                                                    })
+
+                                            })
+                                    })
+
+                            })
+
+                    })
+            })
+    })
+})
+
+app.get('/getCuest', (req, res) => {
     //Lanzamos el query a la base de datos para recuperar todos los cuestionarios
     connect.then(conn => {
         conn.query('SELECT cuestionario.id_cuestionario AS id, cuestionario.nom_cuestionario AS nombre, cuestionario.fec_elaboracion AS fecha, categoria.nom_categoria AS categoria FROM cuestionario JOIN categoria WHERE cuestionario.id_categoria = categoria.id_categoria;')
             .then((rows) => {
                 // Guardamos los cuestionarios recuperados
                 const cuestionarios = rows.map(cuestionario => {
-                    return {id: cuestionario.id, nombre: cuestionario.nombre, fecha: cuestionario.fecha, categoria: cuestionario.categoria}   
+                    return { id: cuestionario.id, nombre: cuestionario.nombre, fecha: cuestionario.fecha, categoria: cuestionario.categoria }
                 })
                 // Regresamos los cuestionarios
                 res.send(cuestionarios);
@@ -149,7 +205,7 @@ app.get('/getStudents', (req, res) => {
             .then(rows => {
                 // Guardamos los alumnos recuperados
                 const alumnos = rows.map(alumno => {
-                    return {id: alumno.id, nombre: alumno.nombre, app: alumno.app, apm: alumno.apm}
+                    return { id: alumno.id, nombre: alumno.nombre, app: alumno.app, apm: alumno.apm }
                 })
                 // Regresamos los alumnos
                 res.send(alumnos)
@@ -168,7 +224,7 @@ app.post('/getEstudiantes', (req, res) => {
             .then(rows => {
                 // Empaquetamos los resultados en un JSON
                 const alumnos = rows.map(alumno => {
-                    return {...alumno}
+                    return { ...alumno }
                 })
                 // Regresamos los alumnos
                 res.send(alumnos)
@@ -177,10 +233,10 @@ app.post('/getEstudiantes', (req, res) => {
 })
 
 app.post('/saveEstudiantesInscritos', (req, res) => {
-    
+
     // Recuperamos el cuerpo de la peticion
-    const {cuestionario, alumnos, fecha} = req.body;
-    
+    const { cuestionario, alumnos, fecha } = req.body;
+
     // Lanzamos el query para inscribir a los estudiantes al cuestionario
     connect.then(conn => {
         // Inscribe a cada alumno al cuestionario seleccionado
@@ -189,6 +245,88 @@ app.post('/saveEstudiantesInscritos', (req, res) => {
         });
         res.send({ succeed: true })
     })
+});
+
+app.post('/getCuestionario', (req, res) => {
+    // Recuperamos el id del usuario al que se inscribio
+    const alumno = req.body.id
+
+    connect.then(conn => {
+        // Recupero el id de la inscripcion al cuestionario
+        conn.query(`SELECT ins_per_cuest.id_ins_per_cuest as 'id_insc', ins_per_cuest.id_cuestionario as 'id_cuest', 
+        cuestionario.nom_cuestionario as 'nom_cuest' FROM ins_per_cuest NATURAL INNER JOIN cuestionario WHERE ${alumno} = ins_per_cuest.id_persona;`)
+            .then(rows => {
+                // Guardo el id de la inscripcion y cuestionario
+                const { id_insc, id_cuest, nom_cuest } = rows[0];
+                // Obtengo los id del registro de pregunta con cuestionario
+                conn.query(`SELECT cuest_pregunta.id_cuest_pregunta as 'id_cq', pregunta.pregunta, pregunta.respA, pregunta.respB, pregunta.respC, pregunta.respD, pregunta.respE,
+                 pregunta.respCorrecta FROM cuest_pregunta NATURAL INNER JOIN pregunta WHERE cuest_pregunta.id_cuestionario = ${id_cuest};`)
+                    .then(rows => {
+                        const json = {
+                            id_ins_per_cuest: id_insc,
+                            nom_cuest,
+                            preguntas: rows.map(pregunta => pregunta)
+                        }
+                        res.send(json)
+                    })
+            })
+    })
+})
+
+app.post('/getRevision', (req, res) => {
+    // obtengo el id del alumno a revisar
+    const alumno = req.body.id;
+
+    connect.then(conn => {
+        // Recupero el cuestionario al que esta inscrito
+        conn.query(`SELECT cuestionario.id_cuestionario as 'id_cuest', cuestionario.nom_cuestionario as 'nom_cuest', 
+        ins_per_cuest.id_ins_per_cuest as 'id_insc' FROM cuestionario NATURAL INNER JOIN ins_per_cuest WHERE id_persona = ${alumno};`)
+            .then(rows => {
+                const { id_cuest, nom_cuest, id_insc } = rows[0];
+                // Recupero todas las preguntas de ese cuestionario 
+                conn.query(`SELECT pregunta.pregunta, pregunta.respCorrecta FROM cuest_pregunta NATURAL INNER JOIN pregunta WHERE id_cuestionario = ${id_cuest};`)
+                    .then(rows => {
+                        const preguntas = rows.map(pregunta => pregunta);
+                        // Recuperamos las respuestas 
+                        conn.query(`SELECT respuesta from resp_per_cuest where id_ins_per_cuest = ${id_insc};`)
+                            .then(rows => {
+                                const respuestas = rows.map(respuesta => respuesta)
+                                // Empaquetamos todo en un JSON
+                                const json = {
+                                    nom_cuest,
+                                    preguntas,
+                                    respuestas
+                                }
+                                res.send(json);
+                            })
+                    })
+            })
+    })
+})
+
+app.post('/saveRespuestas', (req, res) => {
+    // Guardamos el cuerpo del Request
+    const { id_ins_per_cuest, id_cuest_pregunta, respuestas, valor } = req.body;
+    // Calculamos la calificacion del cuestionario
+    let score = 0;
+    valor.forEach(calf => { score = score + calf })
+    // Lanzamos el query para guardar las repuestas del cuestionario
+    connect.then(conn => {
+        // Guardamos las respuestas del cuestionario
+        id_cuest_pregunta.forEach((id, index) => {
+            conn.query(`INSERT INTO resp_per_cuest VALUES(null, ${id_ins_per_cuest}, ${id}, ${JSON.stringify(respuestas[index])}, ${valor[index]})`)
+        })
+        // Actaulizamos la calificacion en el registro de inscripcion
+        conn.query(`UPDATE ins_per_cuest SET cal_cuestionario = ${score / 2} WHERE id_ins_per_cuest = ${id_ins_per_cuest};`)
+            .then(rows => {
+                // Respondemos que las preguntas se guardaron con exito
+                res.send({ succeed: true })
+            })
+    })
+})
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(publicPath, 'index.html'));
 });
 
 app.listen(9000, () => {
